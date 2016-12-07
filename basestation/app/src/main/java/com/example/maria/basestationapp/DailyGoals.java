793230be102878f43ea3f680569ae3ab68befbcf
funnysock <zisserm@gmail.com>
2016-12-07 20:25:39 +0100

    list view and clickable goals added

--- basestation/app/src/main/java/com/example/maria/basestationapp/DailyGoals.java
+++ basestation/app/src/main/java/com/example/maria/basestationapp/DailyGoals.java
@@ -1,13 +1,21 @@
 package com.example.maria.basestationapp;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.example.maria.basestationapp.R;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 /*
 * Entwurf wie Tagesziele dargestellt werden
 * durch Button Plus koennen neue hinzugefuegt werden
@@ -19,17 +27,55 @@ public class DailyGoals extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_daily_goals);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton add = (FloatingActionButton) findViewById(R.id.addGoal);
-        add.setOnClickListener(new View.OnClickListener() {
+
+        final ListView listview = (ListView) findViewById(R.id.goalslistView);
+        String[] values = new String[] { "Obst essen", "Sport machen" };
+
+        final ArrayList<String> list = new ArrayList<String>();
+        for (int i = 0; i < values.length; ++i) {
+            list.add(values[i]);
+        }
+        final StableArrayAdapter adapter = new StableArrayAdapter(this,
+                android.R.layout.simple_list_item_1, list);
+        listview.setAdapter(adapter);
+
+        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onItemClick(AdapterView<?> parent, final View view,
+                                    int position, long id) {
+                final String item = (String) parent.getItemAtPosition(position);
+
             }
+
         });
     }
 
+    private class StableArrayAdapter extends ArrayAdapter<String> {
+
+        HashMap<String, Integer> mIdMap = new HashMap<String, Integer>();
+
+        public StableArrayAdapter(Context context, int textViewResourceId,
+                                  List<String> objects) {
+            super(context, textViewResourceId, objects);
+            for (int i = 0; i < objects.size(); ++i) {
+                mIdMap.put(objects.get(i), i);
+            }
+        }
+
+        @Override
+        public long getItemId(int position) {
+            String item = getItem(position);
+            return mIdMap.get(item);
+        }
+
+        @Override
+        public boolean hasStableIds() {
+            return true;
+        }
+
+    }
+
 }
+
--- basestation/app/src/main/java/com/example/maria/basestationapp/MainActivity.java
+++ basestation/app/src/main/java/com/example/maria/basestationapp/MainActivity.java
@@ -26,8 +26,8 @@ public class MainActivity extends AppCompatActivity {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
-
         setContentView(R.layout.activity_main);
+
 
         ImageButton goal = (ImageButton) findViewById(R.id.goalButton);
         goal.setOnClickListener(new View.OnClickListener() {
