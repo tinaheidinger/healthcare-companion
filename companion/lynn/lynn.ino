396bc09daad59cd44b2a63152faa12e5ef9f6985
tinaheidinger <kristina.heidinger@gmail.com>
2017-01-17 13:57:19 +0100

    Initial commit

---
+++ companion/lynn/lynn.ino
@@ -0,0 +1,73 @@
+#include <Adafruit_GFX.h>    // Core graphics library
+#include <SPI.h>
+#include <Adafruit_ILI9341.h>
+#include "TouchScreen.h"
+
+// These are the four touchscreen analog pins
+#define YP A2  // must be an analog pin, use "An" notation!
+#define XM A3  // must be an analog pin, use "An" notation!
+#define YM 8   // can be a digital pin
+#define XP 9   // can be a digital pin
+
+// This is calibration data for the raw touch data to the screen coordinates
+#define TS_MINX 150
+#define TS_MINY 120
+#define TS_MAXX 920
+#define TS_MAXY 940
+
+#define MINPRESSURE 10
+#define MAXPRESSURE 1000
+
+// The display uses hardware SPI, plus #9 & #10
+#define TFT_CS 13
+#define TFT_DC 12
+Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC);
+
+TouchScreen ts = TouchScreen(XP, YP, XM, YM, 300);
+unsigned long last_gesture = millis();
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  
+  Serial.println(F("All set up!"));
+
+  tft.begin();
+  tft.fillScreen(ILI9341_BLACK);
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  TSPoint p = ts.getPoint();
+
+  // we have some minimum pressure we consider 'valid'
+  // pressure of 0 means no pressing!
+  if (p.z < MINPRESSURE || p.z > MAXPRESSURE) {
+     return;
+  }
+
+  unsigned long current_gesture = millis();
+  p.x = map(p.x, TS_MINX, TS_MAXX, 0, tft.width());
+  p.y = map(p.y, TS_MINY, TS_MAXY, 0, tft.height());
+
+  /*
+  Serial.print("("); Serial.print(p.x);
+  Serial.print(", "); Serial.print(p.y);
+  Serial.println(")");
+  */
+
+  //Serial.println(last_gesture);
+  //Serial.println(current_gesture);
+
+  // is it a continuing or new gesture?
+  if (current_gesture - last_gesture > 10) {
+    // new gesture 
+    Serial.println("new gesture");
+  } else {
+      // continuing gesture  
+  }
+
+  last_gesture = current_gesture;
+
+}
