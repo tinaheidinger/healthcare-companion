5b80d0ca80d0def8a9f28344ec10a0a288993f42
Marie Braun <e1408912@student.tuwien.ac.at>
2017-01-21 01:17:56 +0100

    Slider funktioniert, Graph updated richtig, Graph an Wochenformat angepasst, aktueller Wochentag wird erkannt, Wassermenge wird an akuellem Wochentag über Slider hinzugefügt, Steps-Graph an Wochen-Format angepasst fehlt noch: Monatsansicht, Serverkommunikation

--- basestation/app/src/main/java/com/example/maria/basestationapp/Fluid.java
+++ basestation/app/src/main/java/com/example/maria/basestationapp/Fluid.java
@@ -1,43 +1,149 @@
 package com.example.maria.basestationapp;
 
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.GridLabelRenderer;
+import com.jjoe64.graphview.helper.StaticLabelsFormatter;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
+
+import java.util.Calendar;
 
 /**
  * Created by Marie on 18.01.2017.
  */
 
-
-
 public class Fluid extends AppCompatActivity{
-    private Button backButton;
     private static final String TAG = "FluidScreen";
+
+    private Button backButton;
+    private SeekBar waterAmountSlider;
+    private FloatingActionButton addWaterToDay;
+
+    public int waterAmount = 0;
+    public int dayWaterAmount = 0;
+
+    private int waterMo;
+    private int waterDi;
+    private int waterMi;
+    private int waterDo;
+    private int waterFr;
+    private int waterSa;
+    private int waterSo;
+
+    LineGraphSeries<DataPoint> series;
+
+    public Calendar now = Calendar.getInstance();
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fluid);
 
-        GraphView graph = (GraphView) findViewById(R.id.graph);
-        LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[] {
-                new DataPoint(0, 1),
-                new DataPoint(1, 5),
-                new DataPoint(2, 3)
+        final GraphView graph = (GraphView) findViewById(R.id.graph);
+        GridLabelRenderer glr = graph.getGridLabelRenderer();
+        glr.setPadding(50);
+        series = new LineGraphSeries<>(new DataPoint[] {
+                new DataPoint(0, waterMo),
+                new DataPoint(1, waterDi),
+                new DataPoint(2, waterMi),
+                new DataPoint(3, waterDo),
+                new DataPoint(4, waterFr),
+                new DataPoint(5, waterSa),
+                new DataPoint(6, waterSo)
         });
+
         graph.addSeries(series);
+        StaticLabelsFormatter staticLabelsFormatter = new StaticLabelsFormatter(graph);
+        staticLabelsFormatter.setHorizontalLabels(new String[]{"Mo", "Di", "Mi", "Do", "Fr", "Sa", "So"});
+        graph.getGridLabelRenderer().setLabelFormatter(staticLabelsFormatter);
 
         backButton = (Button) findViewById(R.id.backButtonSteps);
         backButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 onBackPressed();
+
             }
         });
 
+        waterAmountSlider = (SeekBar) findViewById(R.id.waterSlider);
+        waterAmountSlider.setOnSeekBarChangeListener(new waterListener());
+
+        addWaterToDay = (FloatingActionButton) findViewById(R.id.addFluid);
+        addWaterToDay.setOnClickListener(new View.OnClickListener() {
+            public void onClick (View v) {
+                onAddWaterPressed();
+            }
+        });
+    }
+
+    private class waterListener implements SeekBar.OnSeekBarChangeListener {
+        @Override
+        public void onStartTrackingTouch(SeekBar seekBar){}
+
+        @Override
+        public void onStopTrackingTouch(SeekBar seekBar) {}
+
+        @Override
+        public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser){
+            switch (progress){
+                case 0:
+                    waterAmount = 0;
+                    break;
+                case 1:
+                    waterAmount = 200;
+                    break;
+                case 2:
+                    waterAmount = 300;
+                    break;
+                case 3:
+                    waterAmount = 500;
+                    break;
+                case 4:
+                    waterAmount = 750;
+                    break;
+                case 5:
+                    waterAmount = 1000;
+                    break;
+            }
+
+            final TextView waterText = (TextView) findViewById(R.id.addWaterText);
+            waterText.setText(Integer.toString(waterAmount) + " " + getString(R.string.waterText));
+        }
+    }
+
+    public void checkDayOfWeek(){
+        switch(now.get(Calendar.DAY_OF_WEEK)){
+            case(Calendar.MONDAY):
+                waterMo = dayWaterAmount;
+                break;
+            case(Calendar.TUESDAY):
+                waterDi = dayWaterAmount;
+                break;
+            case(Calendar.WEDNESDAY):
+                waterMi = dayWaterAmount;
+                break;
+            case(Calendar.THURSDAY):
+                waterDo = dayWaterAmount;
+                break;
+            case(Calendar.FRIDAY):
+                waterFr = dayWaterAmount;
+                break;
+            case(Calendar.SATURDAY):
+                waterSa = dayWaterAmount;
+                break;
+            case(Calendar.SUNDAY):
+                waterSo = dayWaterAmount;
+                break;
+            default:
+                break;
+        }
     }
 
     public void onBackPressed() {
@@ -45,14 +151,19 @@ public class Fluid extends AppCompatActivity{
         this.finish();
     }
 
-
-
-
-
-        /*int unicode = 0x1F604;
-        String emoji = new String(Character.toChars(unicode));
-
-        titel.setText(emoji);*/
+    public void onAddWaterPressed(){
+        dayWaterAmount += waterAmount;
+        checkDayOfWeek();
+        series.resetData(new DataPoint[] {
+                new DataPoint(0, waterMo),
+                new DataPoint(1, waterDi),
+                new DataPoint(2, waterMi),
+                new DataPoint(3, waterDo),
+                new DataPoint(4, waterFr),
+                new DataPoint(5, waterSa),
+                new DataPoint(6, waterSo)
+        });
 
     }
+}
 
--- basestation/app/src/main/java/com/example/maria/basestationapp/Steps.java
+++ basestation/app/src/main/java/com/example/maria/basestationapp/Steps.java
@@ -6,8 +6,12 @@ import android.view.View;
 import android.widget.Button;
 
 import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.GridLabelRenderer;
+import com.jjoe64.graphview.helper.StaticLabelsFormatter;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
+
+import java.util.Calendar;
 
 /**
  * Created by Marie on 20.01.2017.
@@ -15,19 +19,44 @@ import com.jjoe64.graphview.series.LineGraphSeries;
 
 public class Steps extends AppCompatActivity {
     private Button backButton;
+
     private static final String TAG = "StepsScreen";
+
+    private int stepsMo;
+    private int stepsDi;
+    private int stepsMi;
+    private int stepsDo;
+    private int stepsFr;
+    private int stepsSa;
+    private int stepsSo;
+
+    public int dayStepsAmount;
+
+    LineGraphSeries<DataPoint> series;
+
+    public Calendar now = Calendar.getInstance();
+
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_steps);
 
-        GraphView graph = (GraphView) findViewById(R.id.graph);
-        LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[]{
-                new DataPoint(0, 1),
-                new DataPoint(1, 5),
-                new DataPoint(2, 3)
+        final GraphView graph = (GraphView) findViewById(R.id.graph);
+        GridLabelRenderer glr = graph.getGridLabelRenderer();
+        glr.setPadding(50);
+        series = new LineGraphSeries<>(new DataPoint[] {
+                new DataPoint(0, stepsMo),
+                new DataPoint(1, stepsDi),
+                new DataPoint(2, stepsMi),
+                new DataPoint(3, stepsDo),
+                new DataPoint(4, stepsFr),
+                new DataPoint(5, stepsSa),
+                new DataPoint(6, stepsSo)
         });
         graph.addSeries(series);
+        StaticLabelsFormatter staticLabelsFormatter = new StaticLabelsFormatter(graph);
+        staticLabelsFormatter.setHorizontalLabels(new String[]{"Mo", "Di", "Mi", "Do", "Fr", "Sa", "So"});
+        graph.getGridLabelRenderer().setLabelFormatter(staticLabelsFormatter);
 
         backButton = (Button) findViewById(R.id.backButtonSteps);
         backButton.setOnClickListener(new View.OnClickListener() {
@@ -36,6 +65,34 @@ public class Steps extends AppCompatActivity {
             }
         });
 
+    }
+
+    public void checkDayOfWeek(){
+        switch(now.get(Calendar.DAY_OF_WEEK)){
+            case(Calendar.MONDAY):
+                stepsMo = dayStepsAmount;
+                break;
+            case(Calendar.TUESDAY):
+                stepsDi = dayStepsAmount;
+                break;
+            case(Calendar.WEDNESDAY):
+                stepsMi = dayStepsAmount;
+                break;
+            case(Calendar.THURSDAY):
+                stepsDo = dayStepsAmount;
+                break;
+            case(Calendar.FRIDAY):
+                stepsFr = dayStepsAmount;
+                break;
+            case(Calendar.SATURDAY):
+                stepsSa = dayStepsAmount;
+                break;
+            case(Calendar.SUNDAY):
+                stepsSo = dayStepsAmount;
+                break;
+            default:
+                break;
+        }
     }
 
     public void onBackPressed() {
